1e115fc04f167c405c3b4eba53f0386150a5d642
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-07-11 15:25:02 +0100

    make search bar and address bar responsive

--- components/AddressDetails.tsx
+++ components/AddressDetails.tsx
@@ -1,24 +1,27 @@
 const AddressDetails = () => {
   return (
     <section className="address-details">
-      <ul className="address-details__list">
-        <li className="address-details__item">
+      <div className="address-details__group">
+        <div className="address-details__item">
           <h2 className="address-details__item-title">IP Address</h2>
           <p className="address-details__item-body">192.212.174.101</p>
-        </li>
-        <li className="address-details__item">
+        </div>
+        <div className="address-details__divider"></div>
+        <div className="address-details__item">
           <h2 className="address-details__item-title">Location</h2>
           <p className="address-details__item-body">Brooklyn, NY 10001</p>
-        </li>
-        <li className="address-details__item">
+        </div>
+        <div className="address-details__divider"></div>
+        <div className="address-details__item">
           <h2 className="address-details__item-title">Timezone</h2>
           <p className="address-details__item-body">UTC -05:00</p>
-        </li>
-        <li className="address-details__item">
+        </div>
+        <div className="address-details__divider"></div>
+        <div className="address-details__item">
           <h2 className="address-details__item-title">ISP</h2>
           <p className="address-details__item-body">SpaceX Starlink</p>
-        </li>
-      </ul>
+        </div>
+      </div>
     </section>
   );
 };
